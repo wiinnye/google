592a72bc07aa623c28f37d38e5c6e365dfde12db
wiinnye <wiinnye@gmail.com>
2023-01-05 20:02:46 -0300

    feat: add get in home

---
+++ src/components/Header/Dashboard/HearderDashboard.jsx
@@ -0,0 +1,57 @@
+import { Button, Flex, IconButton, Input, Tooltip } from '@chakra-ui/react';
+import { MdMenu } from 'react-icons/md'
+//import { BsSuitHeartFill } from 'react-icons/bs'
+import { BsSuitHeart } from 'react-icons/bs'
+
+
+export default function HeaderDashboard({ onOpen, isOpen, onClose }) {
+
+  return (
+    <Flex
+      w='full'
+      h='20%'
+      bgColor='yellow.200'
+      direction='row'
+      justify='space-between'
+    >
+      <>
+        <IconButton
+          mt={{ base: '0', sm: '1.5rem', lg: '1rem' }}
+          ml={{ base: '0', sm: '1rem', lg: '1rem' }}
+          onClick={onOpen}
+          icon={<MdMenu />}
+        />
+        <Flex w='50%'>
+          <Input
+            w='60%'
+            mt='1rem'
+            mr='1rem'
+            placeholder='Buscar seu filme aqui...'
+          />
+          <Button
+            w='80px'
+            mt='1rem'
+            mr='1rem'
+          >
+            Buscar
+          </Button>
+          <Button
+            w='150px'
+            mt='1rem'
+            mr='1rem'
+          >
+            Limpar pesquisa
+          </Button>
+          <Tooltip label="Meus Favoritos" aria-label='A tooltip'>
+            <IconButton
+              mt={{ base: '0', sm: '1.5rem', lg: '1rem' }}
+              mr='1rem'
+              onClick={onOpen}
+              icon={<BsSuitHeart />}
+            />
+          </Tooltip>
+        </Flex>
+      </>
+    </Flex>
+  )
+}
--- src/page/Dashboard/Home.jsx
+++ src/page/Dashboard/Home.jsx
@@ -1,8 +1,52 @@
-import { Flex } from '@chakra-ui/react';
 
-export default function Home(){
-  return(
-    <Flex>Home</Flex>
+import HeaderDashboard from '../../components/Header/Dashboard/HearderDashboard';
+import { Container } from '../../layout/container';
+import { Main } from '../../layout/main';
+import FilterHome from '../../components/Drawer';
+import { Flex, Text, useDisclosure } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { CardMovie } from '../../components/Card';
+import { useState } from 'react';
+import { GetNowPlaying } from '../../hook/Dashboard/GetNowPlaying';
 
+export default function Home() {
+  const [movie, setMovie] = useState()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  useEffect(() => {
+    GetNowPlaying(setMovie)
+  }, [movie])
+
+  return (
+    <>
+      <Main>
+        <Container >
+          <HeaderDashboard onOpen={onOpen} />
+          <Flex w='full' h='full' direction='column' >
+            <Flex ml='1rem'>
+              <Text fontSize='2rem'>Ultimos Lançamentos</Text>
+            </Flex>
+            <Flex w='full' wrap='wrap' justify='center'>
+              {movie ?
+                movie.movie.map((item) => (
+                  < CardMovie
+                    ml='1rem'
+                    key={item.id}
+                    img={item.posterPath !== "" ? item.posterPath : 'sem imagem'}
+                    label={item.title !== "" ? item.title : 'sem titulo'}
+                    text={item.overview !== "" ? item.overview : 'sem descrição'}
+                  />
+                ))
+                : <Text> Não temos filmes no momento</Text>
+              }
+            </Flex>
+          </Flex>
+        </Container>
+      </Main>
+      <FilterHome
+        isOpen={isOpen}
+        onClose={onClose}
+      />
+    </>
   )
 }
